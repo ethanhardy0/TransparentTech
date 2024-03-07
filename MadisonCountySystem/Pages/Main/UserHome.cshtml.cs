d493c31007a4c73aa0068741d88415111c9a97ae
Nlynch13 <nicklynch132@gmail.com>
2024-03-07 11:45:46 -0500

    Minor tweaks

--- MadisonCountySystem/Pages/Main/UserHome.cshtml.cs
+++ MadisonCountySystem/Pages/Main/UserHome.cshtml.cs
@@ -11,37 +11,43 @@ namespace MadisonCountySystem.Pages.Main
     {
         public SysUser LoggedUser { get; set; }
         public String? PhotoDir { get; set; }
-        public void OnGet()
+        public IActionResult OnGet()
         {
             // Redirects user if not logged in
             if (HttpContext.Session.GetString("username") == null)
             {
                 HttpContext.Session.SetString("LoginError", "You must login to access that page!");
-                HttpContext.Response.Redirect("/DBLogin");
+                return RedirectToPage("/DBLogin");
             }
-            // Set Library type to Main (No collab selected)
-            HttpContext.Session.SetString("LibType", "Main");
-
-            // Read the logged in user
-            SqlDataReader userReader = DBClass.LoggedUserReader(Int32.Parse(HttpContext.Session.GetString("userID")));
-            LoggedUser = new SysUser();
-
-            // Put into data object
-            if (userReader.Read())
+            else
             {
-                LoggedUser.Username = userReader["Username"].ToString();
-                LoggedUser.Email = userReader["Email"].ToString();
-                LoggedUser.FirstName = userReader["FirstName"].ToString();
-                LoggedUser.LastName = userReader["LastName"].ToString();
-                LoggedUser.Phone = userReader["Phone"].ToString();
-                LoggedUser.UserType = userReader["UserType"].ToString();
+                // Set Library type to Main (No collab selected)
+                HttpContext.Session.SetString("LibType", "Main");
+
+                // Read the logged in user
+                SqlDataReader userReader = DBClass.LoggedUserReader(Int32.Parse(HttpContext.Session.GetString("userID")));
+                LoggedUser = new SysUser();
+
+                // Put into data object
+                if (userReader.Read())
+                {
+                    LoggedUser.Username = userReader["Username"].ToString();
+                    LoggedUser.Email = userReader["Email"].ToString();
+                    LoggedUser.FirstName = userReader["FirstName"].ToString();
+                    LoggedUser.LastName = userReader["LastName"].ToString();
+                    LoggedUser.Phone = userReader["Phone"].ToString();
+                    LoggedUser.UserType = userReader["UserType"].ToString();
+                }
+
+                DBClass.KnowledgeDBConnection.Close();
+
+                // Gets the profile picture directory
+                PhotoDir = DBClass.UserPhotoReader(Int32.Parse(HttpContext.Session.GetString("userID")));
+
+                DBClass.KnowledgeDBConnection.Close();
+
+                return Page();
             }
-
-            DBClass.KnowledgeDBConnection.Close();
-
-            PhotoDir = DBClass.UserPhotoReader(Int32.Parse(HttpContext.Session.GetString("userID")));
-
-            DBClass.KnowledgeDBConnection.Close();
 
         }
     }
