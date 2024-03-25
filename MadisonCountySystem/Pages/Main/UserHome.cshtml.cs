780a1abb1c0e2de0b94935b05f35537abf66e063
Nlynch13 <nicklynch132@gmail.com>
2024-03-25 17:27:22 -0400

    WIP UserHome (Just syncing)

--- MadisonCountySystem/Pages/Main/UserHome.cshtml.cs
+++ MadisonCountySystem/Pages/Main/UserHome.cshtml.cs
@@ -11,6 +11,8 @@ namespace MadisonCountySystem.Pages.Main
     {
         public SysUser LoggedUser { get; set; }
         public String? PhotoDir { get; set; }
+        public List<Collab> Collabs { get; set; }
+        public List<UserCollab> UserCollabs {  get; set; }
         public IActionResult OnGet()
         {
             // Redirects user if not logged in
@@ -43,6 +45,28 @@ namespace MadisonCountySystem.Pages.Main
 
                 // Gets the profile picture directory
                 PhotoDir = DBClass.UserPhotoReader(Int32.Parse(HttpContext.Session.GetString("userID")));
+
+                DBClass.KnowledgeDBConnection.Close();
+
+                SqlDataReader read = DBClass.GeneralReader("SELECT Collaboration.CollabID, CollabName, UserRole FROM UserCollab JOIN Collaboration ON Collaboration.CollabID " +
+                    "= UserCollab.CollabID WHERE UserCollab.UserID = " + HttpContext.Session.GetString("userID") + ";");
+
+                Collabs = new List<Collab>();
+                UserCollabs = new List<UserCollab>();
+
+                while (read.Read())
+                {
+                    Collabs.Add(new Collab
+                    {
+                        CollabName = read["CollabName"].ToString(),
+                        CollabID = Int32.Parse(read["CollabID"].ToString())
+                    });
+
+                    UserCollabs.Add(new UserCollab
+                    {
+                        UserRole = read["UserRole"].ToString()
+                    });
+                }
 
                 DBClass.KnowledgeDBConnection.Close();
 
